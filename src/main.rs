069de0caf3209f3f34b8d551ea6050c7b6cb6e62
vapvin <vins@vins.dev>
2020-08-25 00:29:05 +0900

    Server Rust

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
+#![allow(dead_code)]
+
+use server::Server;
+use std::env;
+use website_handler::WebsiteHandler;
+
+mod http;
+mod server;
+mod website_handler;
+
 fn main() {
-    let server = Server::new("127.0.0.1:8080");
-    server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self{
-        Self {
-            addr
-        }
-    }
-
-    fn run(self){
-        
-    }
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public path: {}", public_path);
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run(WebsiteHandler::new(public_path));
 }
